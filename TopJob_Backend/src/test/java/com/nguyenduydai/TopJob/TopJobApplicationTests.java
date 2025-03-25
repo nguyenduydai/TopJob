aa42e043fb66df5365974d6a11fa560ce16abd33
Nguyen Duy Dai <nguyenduydai5588@gmail.com>
2025-03-25 11:29:00 +0700

    initialize project

---
+++ TopJob_Backend/src/test/java/com/nguyenduydai/TopJob/TopJobApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nguyenduydai.TopJob;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TopJobApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
